97c141c86790ea1edeb32b7324004e4954fe5013
poynting-dev <dixitpriyanshu23@gmail.com>
2021-08-06 22:08:02 +0530

    few modifications

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
 app.get("/", function(req, res) {
   res.render("home", {content: homeStartingContent});
 });
@@ -32,8 +34,12 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  let post = req.body.postTitle;
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 
