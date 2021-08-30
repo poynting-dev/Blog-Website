b5c253137c25cdcc68445ef514f6b1f53425b10b
poynting-dev <dixitpriyanshu23@gmail.com>
2021-08-30 16:40:45 +0530

    correction made to app.js

--- app.js
+++ app.js
@@ -89,6 +89,12 @@ app.get("/contact", function(req, res) {
   res.render("contact", {content: contactContent});
 });
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
