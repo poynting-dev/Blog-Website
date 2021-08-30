a480a408f1173df9de21d161cd65829d5e655607
poynting-dev <dixitpriyanshu23@gmail.com>
2021-08-30 16:16:21 +0530

    Made connection of Blog Website to MongoDB & some style changes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var ld = require('lodash');
+const mongoose = require("mongoose");
+const ld = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +17,68 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 
 app.get("/", function(req, res) {
-  res.render("home", {postsArr: posts});
+  
+  Post.find({}, function(err, post) {
+
+    if(post.length == 0) {
+      const post = new Post({
+        title: "Home",
+        content: homeStartingContent
+      });
+      
+      post.save();
+      res.redirect("/");
+
+    } else {
+      const postTitle = ld.lowerCase(post.title);
+      res.render("home", {
+        startingContent: post.title,
+        postsArr: post
+      });
+    }
+  });    
+  
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
+  });
+
+});
+
+app.get("/posts/:postid", function(req, res) {
+  const requestedId = req.params.postid;
+
+  Post.findById({_id: requestedId}, function(err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
 });
 
 app.get("/about", function(req, res) {
@@ -29,32 +88,6 @@ app.get("/about", function(req, res) {
 app.get("/contact", function(req, res) {
   res.render("contact", {content: contactContent});
 });
-
-app.get("/compose", function(req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
-});
-
-app.get("/posts/:testing", function(req, res) {
-  const paramsTesting = ld.lowerCase(req.params.testing);
-
-  posts.forEach(function(post) {
-    const postTitle = ld.lowerCase(post.title);
-
-    if(postTitle == paramsTesting) {
-      res.render("post", {postHead: post.title, postContent: post.content});
-    }
-  });
-});
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
