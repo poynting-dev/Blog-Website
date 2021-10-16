c2d7139444d2fd3f7740665976f7d91689def343
poynting-dev <dixitpriyanshu23@gmail.com>
2021-10-16 14:02:10 +0530

    Added Update Blog option

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const ld = require("lodash");
+const methodOverride = require("method-override");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,6 +17,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb+srv://admin-priyanshu:Strength100@cluster0.nuayu.mongodb.net/blogDB", {useNewUrlParser: true});
 
@@ -80,6 +82,32 @@ app.get("/posts/:postid", function(req, res) {
   });
 
 });
+
+//Get EditForm
+app.get("/:postid/edit", function(req, res) {
+  Post.findById({_id: req.params.postid}, function(err, post) {
+      if(err){
+          console.log(err);
+          res.redirect("/");
+      }else{
+        res.render("update", {
+          post: post
+        });
+      }
+  });
+});
+
+app.put("/:postid/edit",(req, res)=>{
+  Post.findByIdAndUpdate(req.params.postid, {content: req.body.postBody}, function(err,updatedata){
+      if(err){
+          console.log(err);
+          res.redirect("/");
+      }else{
+          res.redirect("/");
+      }
+  })
+})
+
 
 app.get("/about", function(req, res) {
   res.render("about", {content: aboutContent});
